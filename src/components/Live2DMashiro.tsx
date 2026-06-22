bf0ea875ea2cab40c56b64f5d6e73a21a58c0912
万晨阳 <1541148313@qq.com>
2026-06-22 13:54:22 +0800

    切换 Mashiro Live2D 为夏季校服（live2d2）

--- src/components/Live2DMashiro.tsx
+++ src/components/Live2DMashiro.tsx
@@ -114,7 +114,7 @@ export function Live2DMashiro() {
         container!.appendChild(app.view as HTMLCanvasElement);
 
         const model = await Live2DModel.from(
-          "/mashiro live2d/mashiro.model.json"
+          "/live2d2/mashiro.model.json"
         );
         if (cancelled) {
           app.destroy(true, { children: true });
